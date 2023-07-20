18dacdfc63604778cf19387981ac14db8431141f
админ <bolottashirkulov@gmail.com>
2023-07-20 13:27:43 +0600

    git init begin project

---
+++ src/main/java/kg/attractor/eventsubsservice/EventSubsServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.eventsubsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSubsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSubsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/eventsubsservice/EventSubsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.eventsubsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSubsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
